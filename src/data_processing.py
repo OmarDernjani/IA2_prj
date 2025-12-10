ecd2308d4ded16e433ff2e9a231c9a468a55b305
OmarDernjani <omar.dernjani4@gmail.com>
2025-12-10 11:51:51 +0100

    updated src

--- src/data_processing.py
+++ src/data_processing.py
@@ -30,5 +30,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, target, random_state = 42
 X_train.to_csv('data/processed/X_train_MLB.csv')
 X_test.to_csv('data/processed/X_test_MLB.csv')
 y_train.to_csv('data/processed/y_train.csv')
-y_test.to_csv('data/preprocessed/y_test.csv')
+y_test.to_csv('data/processed/y_test.csv')
 
